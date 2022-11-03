350d6b163df87c201fa25ef7263372578e02b57f
Josh <joshuamyers1097@gmail.com>
2022-11-03 19:52:13 +0000

    changed to mpu6050, fixed setup

--- flight-computer/flight-computer.ino
+++ flight-computer/flight-computer.ino
@@ -1,15 +1,57 @@
-#include <mpu9250.h>
+#include <Adafruit_MPU6050.h>
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
+Adafruit_MPU6050 mpu;
+Adafruit_BMP085 bmp;
+#define seaLevelPressure_hPa //input here
 
 void setup() {
   // put your setup code here, to run once:
-
+  Wire.begin();
+  SD.begin(4); // change pin later to match with the SD card reading
+  Wire.setClock(400000);
+  Serial.begin(9600);
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    }
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {
+      delay(10);
+    }
+  }
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // bpm reading methods
+  /* 
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+  
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
 
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(seaLevelPressure_hPa * 100));
+  Serial.println(" meters");
+  
+  Serial.println();
+  delay(500);
+  */
+  
 }
