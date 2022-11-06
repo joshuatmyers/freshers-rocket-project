aee7c95c17bc9e82ee898fa978c279e16b8c8821
Josh <joshuamyers1097@gmail.com>
2022-11-06 16:54:27 +0000

    mpu6050 readings

--- flight-computer/flight-computer.ino
+++ flight-computer/flight-computer.ino
@@ -23,8 +23,15 @@ void setup() {
       delay(10);
     }
   }
-  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  // set accelerometer range to +-16G
+	mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+
+	// set gyro range to +- 500 deg/s
+	mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+	// set filter bandwidth to 21 Hz
+	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   
 }
@@ -33,7 +40,32 @@ void loop() {
   // put your main code here, to run repeatedly:
   // bpm reading methods
   
-   
+  sensors_event_t a, g, temp;
+	mpu.getEvent(&a, &g, &temp);
+  Serial.print("Acceleration X: ");
+	Serial.print(a.acceleration.x);
+	Serial.print(", Y: ");
+	Serial.print(a.acceleration.y);
+	Serial.print(", Z: ");
+	Serial.print(a.acceleration.z);
+	Serial.println(" m/s^2");
+
+	Serial.print("Rotation X: ");
+	Serial.print(g.gyro.x);
+	Serial.print(", Y: ");
+	Serial.print(g.gyro.y);
+	Serial.print(", Z: ");
+	Serial.print(g.gyro.z);
+	Serial.println(" rad/s");
+
+	Serial.print("Temperature: ");
+	Serial.print(temp.temperature);
+	Serial.println(" degC");
+
+	Serial.println("");
+	delay(100);
+
+  /* 
   Serial.print("Temperature = ");
   Serial.print(bmp.readTemperature());
   Serial.println(" *C");
@@ -56,6 +88,7 @@ void loop() {
   
   Serial.println();
   delay(500);
+  */
   
   
 }
